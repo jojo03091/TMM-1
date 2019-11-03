ccf5060ebcf6aa2fed4d23633d95a8d36c2efff2
junzhe0213 <gen199999@gmail.com>
2019-11-03 18:01:39 +0800

    Camera轉動 角色跟著左右旋轉 移動

---
+++ The_Malleus_Maleficarum/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private LookMouse LM;
+
+    public float WalkSpeed = 6.0F;
+    public float RunSpeed = 8.0F;
+
+    void Start()
+    {
+        LM = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<LookMouse>();
+    }
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.Euler(0, LM.yRotation, 0);
+        MoveMent();
+    }
+
+    void MoveMent()
+    {
+        float horizontal = Input.GetAxis("Horizontal"); //A D 左右
+        float vertical = Input.GetAxis("Vertical"); //W S 上 下
+
+        transform.Translate(Vector3.forward * vertical * WalkSpeed * Time.deltaTime);//W S 上 下
+        transform.Translate(Vector3.right * horizontal * WalkSpeed * Time.deltaTime);//A D 左右
+    }
+}
